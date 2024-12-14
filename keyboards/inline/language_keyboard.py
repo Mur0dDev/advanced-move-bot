1f782d68b9d01cde21fa0547cd299494b4f684ce
Mur0dDev <msadullaevdev@gmail.com>
2024-12-14 18:52:51 -0500

    i am planning to use another sytle for language_keyboard

--- keyboards/inline/language_keyboard.py
+++ keyboards/inline/language_keyboard.py
@@ -1,13 +1,13 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-def get_language_keyboard():
-    return InlineKeyboardMarkup(inline_keyboard=[
-        [
-            InlineKeyboardButton(text="🇺🇸 English", callback_data="set_language_en"),
-            InlineKeyboardButton(text="🇺🇿 Uzbek", callback_data="set_language_uz"),
-            InlineKeyboardButton(text="🇷🇺 Russian", callback_data="set_language_ru")
-        ],
-        [
-            InlineKeyboardButton(text="❌ Close", callback_data="close_settings")
-        ]
-    ])
+# from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+#
+# def get_language_keyboard():
+#     return InlineKeyboardMarkup(inline_keyboard=[
+#         [
+#             InlineKeyboardButton(text="🇺🇸 English", callback_data="set_language_en"),
+#             InlineKeyboardButton(text="🇺🇿 Uzbek", callback_data="set_language_uz"),
+#             InlineKeyboardButton(text="🇷🇺 Russian", callback_data="set_language_ru")
+#         ],
+#         [
+#             InlineKeyboardButton(text="❌ Close", callback_data="close_settings")
+#         ]
+#     ])
